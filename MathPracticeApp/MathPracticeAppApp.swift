07a6d2df39f8a3cbdf944186a5dcdbb994221180
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-02 09:44:34 -0500

    Initial Commit

---
+++ MathPracticeApp/MathPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathPracticeAppApp.swift
+//  MathPracticeApp
+//
+//  Created by Mohamed, Ilana on 2021-03-02.
+//
+
+import SwiftUI
+
+@main
+struct MathPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
